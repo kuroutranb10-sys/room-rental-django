a525a7cfb57f312512d404715a6629e4b77d2942
kuroutran0612 <117264956+kuroutran0612@users.noreply.github.com>
2026-09-04 22:38:46 +0700

    Add homepage redirect

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+def home(request):
+    return redirect('room_list')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rooms/', include('rooms.urls')),
+    path('', home),
 ]
 
 urlpatterns += static(
